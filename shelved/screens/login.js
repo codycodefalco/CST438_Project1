eb59d93b17369cb0914cbe458b32fc7b32ae4e0e
mikalagumbay <129779813+mikalagumbay@users.noreply.github.com>
2024-09-05 18:32:27 -0700

    WIP: Setup for log in, sign up, and welcome pages

---
+++ shelved/screens/login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+import{
+    StyledContainer,
+    InnerContainer,
+    PageLogo,
+    PageTitle
+} from '../components/styles';
+
+const Login = () => {
+    return (
+        <StyledContainer>
+            <StatusBar style="dark"/>
+            <InnerContainer>
+                <PageLogo resizeMode="cover" source={require('./../assets/img/favicon.png')}/>
+                <PageTitle> Shelved </PageTitle> 
+            </InnerContainer>
+        </StyledContainer>
+    );
+}
+
+export default Login;
