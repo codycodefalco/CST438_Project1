2d94627116fd1389c8ab4080526c416f129f8a97
Gerardo Solis <118018647+Jerry-912@users.noreply.github.com>
2024-09-07 16:21:52 -0700

    Started work on Google Oauth

--- shelved/App.js
+++ shelved/App.js
@@ -1,11 +1,28 @@
+// andriod 737955579318-q1re4v9dlp5tf7u9blh0vu1knmealk2c.apps.googleusercontent.com
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Google from 'expo-auth-session/providers/google';
 
 // screens
 import Login from './screens/login.js';
 
 export default function App() {
-  return <Login/>
+  const [userInfo, setUserInfo] = React.useState(null);
+  const [request,response, promptAsync] = Google.useAuthRequest({
+    androidClientId: "737955579318-q1re4v9dlp5tf7u9blh0vu1knmealk2c.apps.googleusercontent.com"
+  });
+
+  async function handleSingInWithGoogle() {
+    await AsyncStorage.getI
+  }
+  return (
+    <View style={styles.container}>
+      <Text>Sign in </Text>
+      <Button title ="Google Sign In" onPress={promptAsync}></Button>
+
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
